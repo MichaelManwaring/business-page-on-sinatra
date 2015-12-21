2edd3739b42f79b01245f8550f397624369eae95
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2015-12-20 20:39:22 -0500

    finally resolved conflicts

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 ruby '2.2.3'
 gem 'sinatra'
+gem 'sendgrid-ruby'
--- public/js/vino.js
+++ public/js/vino.js
@@ -78,5 +78,63 @@ $(".menu_lk").hover(function() {
   }
 });
 
+function getAge(birthdate) {
+    var today = new Date();
+    var birthDate = new Date(birthdate);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 
+// Form testing
+  // $( "input" ).change(function( event ) {
+    // console.log("change to form");
+  // });
+  subm = false
+  // ensure key fields are prepared for submission
+  submessage = ""
+  $(".password").change(function( event ){
+    if($(".password").val().length < 6){
+      var submessage = 'password cannot be less than 6 characters'
+    }else{
+      var submessage = ""
+    };
+    console.log(submessage)
+  });
+  $("#password2").change(function( event ){
+    if($("#password").val() != $("#password2").val()){
+      var submessage = 'The two passwords you provided do not match.'
+    }else{
+      var submessage = ""
+    };
+    console.log(submessage)
+  });
+  $("#birthdate").change(function( event ){
+    if(getAge($('#birthdate').val()) < 21 ){
+      var submessage="It is illegal for persons under 21 years of age to purchase alchohol"
+    }else if (getAge($('#birthdate').val()) >110) {
+      var submessage = "This age is suspiciously old"
+    }else{
+      var submessage = ""
+    }
+    console.log(submessage)
+  });
+// hiding the secondary adress bar untill requested
+  $("#shipping").hide();
+  $('#gift').change(function() {
+        $('#shipping').show();
+      var gift_toggle = true;
+    console.log(gift_toggle)
+    });
+// Final check for submission
+  $( "form" ).submit(function( event ) {
+    if(submessage === "") {
+      return;
+    }
+    $( "#validform" ).text( submessage ).show().fadeOut( 2000 );
+    event.preventDefault();
+  });
 });
